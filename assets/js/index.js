9258573e7af4410feeb1d76aa7cac9b2409c192a
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-03-10 00:25:04 +0300

    Es linters err fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,65 +1,63 @@
 const ourBlogers = [
   {
-    blogerPicture: "./assets/images/blogers/blogger1.jpg",
-    blogerName: "James Morining",
-    blogerposition: "Health toplics Bloger ",
+    blogerPicture: './assets/images/blogers/blogger1.jpg',
+    blogerName: 'James Morining',
+    blogerposition: 'Health toplics Bloger ',
     article:
-      "The start of a new decade brings with it new resolutions to improve one’s life, including a healthier lifestyle. ",
+      'The start of a new decade brings with it new resolutions to improve one’s life, including a healthier lifestyle. ',
   },
   {
-    blogerPicture: "./assets/images/blogers/blogger2.jpg",
-    blogerName: "Julia Jonson",
-    blogerposition: "Food Blogger",
+    blogerPicture: './assets/images/blogers/blogger2.jpg',
+    blogerName: 'Julia Jonson',
+    blogerposition: 'Food Blogger',
     article:
-      "Variety of recipes and cooking-related articles with a focus on thoughtful and stylish living. ",
+      'Variety of recipes and cooking-related articles with a focus on thoughtful and stylish living. ',
   },
   {
-    blogerPicture: "./assets/images/blogers/blogger3.jpg",
-    blogerName: "Smith Aniken",
-    blogerposition: "News Blogger",
+    blogerPicture: './assets/images/blogers/blogger3.jpg',
+    blogerName: 'Smith Aniken',
+    blogerposition: 'News Blogger',
     article:
-      "In essence, a news blog is a type of news-publishing platform that mainly generates written and current-stories content",
+      'In essence, a news blog is a type of news-publishing platform that mainly generates written and current-stories content',
   },
   {
-    blogerPicture: "./assets/images/blogers/blogger4.jpg",
-    blogerName: "Jenefer Bond",
-    blogerposition: "News blogger",
+    blogerPicture: './assets/images/blogers/blogger4.jpg',
+    blogerName: 'Jenefer Bond',
+    blogerposition: 'News blogger',
     article:
-      "Here we bring you All the latest news about Ethiopia from the CES blogs.",
+      'Here we bring you All the latest news about Ethiopia from the CES blogs.',
   },
   {
-    blogerPicture: "./assets/images/blogers/blogger5.jpg",
-    blogerName: "Eve Anston",
-    blogerposition: "Fashion Bloger",
+    blogerPicture: './assets/images/blogers/blogger5.jpg',
+    blogerName: 'Eve Anston',
+    blogerposition: 'Fashion Bloger',
     article:
-      "A fashion blog can cover many topics, such as specific items of clothing and accessories, and beauty tips ",
+      'A fashion blog can cover many topics, such as specific items of clothing and accessories, and beauty tips ',
   },
   {
-    blogerPicture: "./assets/images/blogers/blogger6.jpg",
-    blogerName: "Sentayhu Berhanu",
-    blogerposition: "Tech blogger",
+    blogerPicture: './assets/images/blogers/blogger6.jpg',
+    blogerName: 'Sentayhu Berhanu',
+    blogerposition: 'Tech blogger',
     article:
-      "Discover your best self through daily tips, quotes and new articles posted weekly. ",
+      'Discover your best self through daily tips, quotes and new articles posted weekly. ',
   },
 ];
-const hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", () => {
-  document.getElementById("cover").style.cssText =
-    "display: block; position: fixed; z-index: 5; width: 100%; height: 100%; background-color: #272a31; opacity: 0.9; top:0; left:0; mix-blend-mode: multiply; backdrop-filter: blur(6px);";
-  document.getElementById("close").style.cssText = "display: block;";
-  document.getElementById("body").style.cssText = "overflow:hidden;";
-  document.getElementById("menu").style.cssText =
-    "display:block;  display: flex;";
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', () => {
+  document.getElementById('cover').style.cssText = 'display: block; position: fixed; z-index: 5; width: 100%; height: 100%; background-color: #272a31; opacity: 0.9; top:0; left:0; mix-blend-mode: multiply; backdrop-filter: blur(6px);';
+  document.getElementById('close').style.cssText = 'display: block;';
+  document.getElementById('body').style.cssText = 'overflow:hidden;';
+  document.getElementById('menu').style.cssText = 'display:block;  display: flex;';
 });
-const close = document.querySelector("#close");
-close.addEventListener("click", () => {
-  document.getElementById("cover").style.cssText = "display:none; ";
-  document.getElementById("body").style.cssText = "overflow:none;";
-  document.getElementById("menu").style.cssText = "display:none";
+const close = document.querySelector('#close');
+close.addEventListener('click', () => {
+  document.getElementById('cover').style.cssText = 'display:none; ';
+  document.getElementById('body').style.cssText = 'overflow:none;';
+  document.getElementById('menu').style.cssText = 'display:none';
 });
 function bloggersList() {
-  let renderAll = "";
-  const renderblogger = document.getElementById("our-blogers");
+  let renderAll = '';
+  const renderblogger = document.getElementById('our-blogers');
   ourBlogers.forEach((blogger) => {
     const render = `<div class="blogger-data">
     <div>
